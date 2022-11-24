2cad7e784cc19777804a186d78e9f0af142a97fd
Amauryrdzrz <amauryrdzrz@gmail.com>
2022-11-24 08:13:43 -0600

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const weather = require('./utils/weather')
 const hbs = require('hbs')
 
 const app = express()
-
-//define paths for express config
+const port = process.env.PORT || 3000
+    //define paths for express config
 const publicDP = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -107,6 +107,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up in port 3000...')
+app.listen(port, () => {
+    console.log('Server is up in port ' + port)
 })
